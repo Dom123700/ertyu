9c019d0d29b355d4653633c30c18a09d9b95370d
Dom123700 <156134586+Dom123700@users.noreply.github.com>
2024-01-20 19:07:11 +0000

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -11,7 +11,7 @@ module.exports.run = async (client, message, args) => {
         new MessageButton()
         .setLabel('Support Server')
         .setStyle('LINK')
-        .setURL("https://discord.gg/ARu4hr6hJw"),
+        .setURL("https://discord.gg/dvvp5Xb8R5"),
     )
     let invite = new MessageEmbed()
     .setAuthor(`Invite ${client.user.username} `, client.user.avatarURL())
