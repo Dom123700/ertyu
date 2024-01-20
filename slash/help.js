0c8844263539855056345f4d7ccfa58d48f01fc0
Dom123700 <156134586+Dom123700@users.noreply.github.com>
2024-01-20 18:41:43 +0000

    Update help.js

--- slash/help.js
+++ slash/help.js
@@ -8,7 +8,6 @@ module.exports = {
         .setTitle(`Commands of ${client.user.username}`)
         .setColor('#2F3136')
         .setDescription('**Please Select a category to view all its commands**')
-        .addField(`Links:`,`- [Youtube Channel](https://youtube.com/c/Zerosync)\n- [Discord Server](https://discord.gg/ARu4hr6hJw)\n- [GitHub](https://github.com/ZeroDiscord/Giveaway)`,true)
         .setTimestamp()
         .setFooter(`Requested by ${interaction.user.username} | GiveawayBot™ v3 By ZeroSync`, interaction.user.displayAvatarURL());
         
