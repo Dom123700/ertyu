911882fa1184c1c3fef37f517d0a77250902ab7b
Dom123700 <156134586+Dom123700@users.noreply.github.com>
2024-01-20 18:48:32 +0000

    Update giveawayEnded.js

--- events/giveaways/giveawayEnded.js
+++ events/giveaways/giveawayEnded.js
@@ -6,7 +6,7 @@ module.exports = {
         embeds: [new Discord.MessageEmbed()
           .setTitle(`🎁 Let's goo!`)
           .setColor("#2F3136")
-          .setDescription(`Hello there ${member.user}\n I heard that you have won **[[This Giveaway]](https://discord.com/channels/${giveaway.guildId}/${giveaway.channelId}/${giveaway.messageId})**\n Good Job On Winning **${giveaway.prize}!**\nDirect Message the host to claim your prize!!`)
+          .setDescription(`Hello there ${member.user}\n You have won **[[This Giveaway]](https://discord.com/channels/${giveaway.guildId}/${giveaway.channelId}/${giveaway.messageId})**\n Good Job On Winning **${giveaway.prize}!**\nDirect Message the host to claim your prize!!`)
           .setTimestamp()
           .setFooter(member.user.username, member.user.displayAvatarURL())
         ]
